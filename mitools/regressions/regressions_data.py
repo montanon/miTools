6d300c1e03120ae64b00ab07990f66636bbc3f11
montanon <montagna.p.92@hotmail.com>
2023-10-29 22:50:36 +0900

    first CTReg structure

--- mitools/regressions/regressions_data.py
+++ mitools/regressions/regressions_data.py
@@ -7,15 +7,102 @@ from abc import ABC
 
 
 class RegressionData(ABC):
-    pass
+    @abstractmethod
+    def __post_init__(self):
+        pass
+    @abstractmethod
+    def to_df(self):
+        pass
+    @abstractmethod
+    def to_pretty_df(self):
+        pass
 
 def format_result(row):
     return f"{row.round(2)} ({row.astype(str)})"
 
+@dataclass(frozen=True)
+class XTRegResults(RegressionData):
+
+    n_obs: int
+    n_groups: int
+    n_obs_per_group: int
+    F_stats: Dict[Tuple[int, int],float]
+    Prob_F: int
+    R_sq: float
+    R_sq_within: float
+    R_sq_between: float
+    corr = float
+
+    dep_variable: str
+    indep_variables: List[str]
+
+    coefficients: Dict[str,float]
+    std_errs: Dict[str,float]
+    t_values: Dict[str,float]
+    p_values: Dict[str,float]
+    significances: Dict[str,str]
+    conf_interval: Dict[str,Tuple[float,float]]
+
+    model_params: Dict[str,Any]
+    model_specification: str
+
+    coefficient_col: str = 'Coefficient'
+    significance_col: str = 'P>|t|'
+
+    def __post__init__(self):
+        assert all(v in self.coefficients.keys() for v in self.indep_variables)
+        assert all(v in self.std_errs.keys() for v in self.indep_variables)
+        assert all(v in self.t_values.keys() for v in self.indep_variables)
+        assert all(v in self.p_values.keys() for v in self.indep_variables)
+        assert all(v in self.significances.keys() for v in self.indep_variables)
+        assert all(v in self.conf_interval.keys() for v in self.indep_variables)
+
+    def to_df(self):
+        
+        base_data = {
+        'n_obs':self.n_obs,
+        'n_groups': self.n_groups,
+        'n_obs_per_group': self.n_obs_per_group,
+        'F_stats': self.F_stats,
+        'Prob_F': self.Prob_F,
+        'R_sq': self.R_sq,
+        'R_sq_within': self.R_sq_within,
+        'R_sq_between': self.R_sq_between,
+        'model_specification': self.model_specification
+        }
+
+        rows = []
+        for var, stats in self.coefficients.items():
+            row = {'Variable': var}
+            row.update(stats)
+            rows.append(row)
+        df = DataFrame(rows)
+        for key, value in base_data.items():
+            df[key] = value
+        relevant_cols = ['Variable', self.coefficient_col, self.significance_col]
+        df = df[relevant_cols]
+        df['Dep Var'] = self.dep_variable
+        df = df.set_index('Dep Var')
+        df = df.set_index('Variable', append=True)
+        if self.indep_variables == 0:
+            self.indep_variables = [str(self.indep_variables)] # YIKES
+        df['Indep Var'] = [v for v in self.indep_variables if v.find('.') == -1][0]
+        df = df.set_index('Indep Var', append=True)
+        return df
+    
+    def to_pretty_df(self):
+        regression_data = self.to_df()
+        regression_data['Result'] = (regression_data[self.coefficient_col]
+                                    .round(2)
+                                    .astype(str) + ' (' + regression_data[self.significance_col]
+                                    .astype(str) + ')')
+        regression_data['Result'] = regression_data['Result'].apply(add_significance)
+        regression_data = regression_data[['Result']]
+        return regression_data
+
+
 @dataclass(frozen=False)
 class OLSResults(RegressionData):
-
-    model_params: Dict
 
     n_obs: int
     F_stats: Dict[Tuple[int, int],float]
