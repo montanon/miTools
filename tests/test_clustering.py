5e178c80b5fcac9e8f100afb404f4d14090001f5
montanon <montagna.p.92@hotmail.com>
2023-11-04 06:59:04 +0900

    Finished sql tests

--- tests/test_clustering.py
+++ tests/test_clustering.py
@@ -8,7 +8,7 @@ from sklearn.datasets import make_blobs
 from sklearn.neighbors import NearestCentroid
 
 
-class TestGetDistancesToCentroids(.TestCase):
+class TestGetDistancesToCentroids(TestCase):
 
     def setUp(self):
         # Mock data setup
@@ -216,6 +216,90 @@ class TestAgglomerativeClustering(TestCase):
             agglomerative_clustering(self.data, 0)
         with self.assertRaises(ValueError):  # Assuming it raises a ValueError for invalid n_clusters
             agglomerative_clustering(self.data, -1)
+
+
+class TestAgglomerativeNClusterSearch(TestCase):
+
+    def setUp(self):
+        # Mock data setup: Create datasets with blobs
+        self.data, _ = make_blobs(n_samples=100, centers=4, random_state=42)
+        self.data = DataFrame(self.data, columns=['x', 'y'])
+
+    def test_positive_case(self):
+        max_clusters = 5
+        result = agglomerative_ncluster_search(self.data, max_clusters)
+        
+        # Ensure function returns scores for all cluster counts from 2 to max_clusters - 1
+        self.assertEqual(len(result), max_clusters - 2)
+        
+    def test_empty_dataframe(self):
+        # Ensure function handles an empty dataframe without errors
+        empty_data = DataFrame(columns=['x', 'y'])
+        with self.assertRaises(ValueError):  # Assuming it raises a ValueError due to empty data
+            agglomerative_ncluster_search(empty_data, 10)
+
+    def test_invalid_max_clusters(self):
+        # Ensure function raises an error with invalid max_clusters value
+        with self.assertRaises(ValueError):  # Assuming it raises a ValueError for invalid max_clusters
+            agglomerative_ncluster_search(self.data, 1)
+
+
+class TestKMeansClustering(TestCase):
+
+    def setUp(self):
+        # Mock data setup: Create datasets with blobs that can be clustered
+        self.data, _ = make_blobs(n_samples=100, centers=3, random_state=42)
+        self.data = DataFrame(self.data, columns=['x', 'y'])
+
+    def test_positive_case(self):
+        n_clusters = 3
+        result = kmeans_clustering(self.data, n_clusters)
+        # Ensure function returns labels for all data points
+        self.assertEqual(len(result), len(self.data))
+        # Ensure the number of unique labels equals n_clusters
+        unique_labels = unique(result)
+        self.assertEqual(len(unique_labels), n_clusters)
+
+    def test_empty_dataframe(self):
+        # Ensure function handles an empty dataframe without errors
+        empty_data = DataFrame(columns=['x', 'y'])
+        with self.assertRaises(ValueError):  # Assuming it raises a ValueError due to empty data
+            kmeans_clustering(empty_data, 1)
+
+    def test_invalid_n_clusters(self):
+        # Ensure function raises an error with invalid n_clusters value
+        with self.assertRaises(ValueError):  # Assuming it raises a ValueError for invalid n_clusters
+            kmeans_clustering(self.data, 0)
+        with self.assertRaises(ValueError):  # Assuming it raises a ValueError for invalid n_clusters
+            kmeans_clustering(self.data, -1)
+
+
+class TestKMeansNClusterSearch(TestCase):
+
+    def setUp(self):
+        # Mock data setup: create datasets with blobs that can be clustered
+        self.data, _ = make_blobs(n_samples=100, centers=4, random_state=42)
+        self.data = DataFrame(self.data, columns=['x', 'y'])
+
+    def test_positive_case(self):
+        max_clusters = 4
+        silhouette_scores, inertia_values = kmeans_ncluster_search(self.data, max_clusters)
+        
+        # Ensure function returns scores and inertia for all cluster counts from 2 to max_clusters - 1
+        self.assertEqual(len(silhouette_scores), max_clusters - 2)
+        self.assertEqual(len(inertia_values), max_clusters - 2)
+        
+    def test_empty_dataframe(self):
+        # Ensure function handles an empty dataframe without errors
+        empty_data = DataFrame(columns=['x', 'y'])
+        with self.assertRaises(ValueError):  # Assuming it raises a ValueError due to empty data
+            kmeans_ncluster_search(empty_data, 10)
+
+    def test_invalid_max_clusters(self):
+        # Ensure function raises an error with invalid max_clusters value
+        with self.assertRaises(ValueError):  # Assuming it raises a ValueError for invalid max_clusters
+            kmeans_ncluster_search(self.data, 1)
+
 
 if __name__ == "__main__":
     unittest.main()
