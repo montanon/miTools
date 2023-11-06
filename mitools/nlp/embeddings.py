4e77b94ac8ebbf0a3fd9c280123ff1ccfa544667
montanon <montagna.p.92@hotmail.com>
2023-11-06 11:59:51 +0900

    fixed some typing, added returns typing

--- mitools/nlp/embeddings.py
+++ mitools/nlp/embeddings.py
@@ -1,10 +1,11 @@
 import warnings
 from ast import literal_eval
-from typing import List, Optional, Union
+from typing import Callable, Iterable, List, Optional, Union
 
+from nltk.tokenize.api import StringTokenizer
 from numba.core.errors import (NumbaDeprecationWarning,
                                NumbaPendingDeprecationWarning)
-from numpy import float64
+from numpy import float64, ndarray
 from pandas import DataFrame, Series
 from tqdm import tqdm
 
@@ -19,7 +20,8 @@ SPECTER_EMBEDDINGS_URL = "https://model-apis.semanticscholar.org/specter/v1/invo
 MAX_BATCH_SIZE = 4
 
 
-def huggingface_specter_embed_texts(texts: Union[List[str],str], batch_size: Optional[int]=MAX_BATCH_SIZE, n_threads: Optional[int]=1):
+def huggingface_specter_embed_texts(texts: Union[List[str],str], batch_size: Optional[int]=MAX_BATCH_SIZE, 
+                                    n_threads: Optional[int]=1) -> List[ndarray]:
     tokenizer = AutoTokenizer.from_pretrained('allenai/specter')
     model = AutoModel.from_pretrained('allenai/specter')
     
@@ -32,27 +34,28 @@ def huggingface_specter_embed_texts(texts: Union[List[str],str], batch_size: Opt
             embeddings.extend(huggingface_specter_embed_chunk(chunk, tokenizer, model))
         return embeddings
 
-def huggingface_specter_embed_texts_parallel(chunk, tokenizer, model):
+def huggingface_specter_embed_texts_parallel(chunk: Iterable, tokenizer: StringTokenizer, model: AutoModel) -> Callable:
     return huggingface_specter_embed_chunk(chunk, tokenizer, model)
 
-def huggingface_specter_embed_chunk(chunk, tokenizer, model):
+def huggingface_specter_embed_chunk(chunk: Iterable, tokenizer: StringTokenizer, model: AutoModel) -> List[ndarray]:
     inputs = tokenizer(chunk, padding=True, truncation=True, return_tensors="pt", max_length=512)
     result = model(**inputs)
     return result.last_hidden_state[:, 0, :].detach().numpy().tolist()
 
-def semantic_scholar_specter_embed_texts(texts: Union[List[str],str], batch_size: Optional[int]=MAX_BATCH_SIZE):
+def semantic_scholar_specter_embed_texts(texts: Union[List[str],str], batch_size: Optional[int]=MAX_BATCH_SIZE
+                                         ) -> List[ndarray]:
     embeddings = []
     for chunk in tqdm(iterable_chunks(texts, batch_size), total=len(texts)/batch_size):
         pass
     return embeddings
 
-def embeddings_col_to_frame(embeddings: Series):
+def embeddings_col_to_frame(embeddings: Series) -> DataFrame:
     if all(isinstance(value, str) for value in embeddings):
         embeddings = embeddings.apply(literal_eval)
     embeddings = (embeddings.apply(Series)
                   .astype(float64))
     return embeddings
 
-def umap_embeddings(embeddings: DataFrame):
+def umap_embeddings(embeddings: DataFrame) -> ndarray:
     reducer = UMAP()
     return reducer.fit_transform(embeddings)
