0f3b3b8c81387105a05483a56d354b6ee1b0e23f
montanon <montagna.p.92@hotmail.com>
2023-11-15 18:41:41 +0900

    added __init__ for mitools superpackage

---
+++ mitools/__init__.py
@@ -0,0 +1,29 @@
+def _setup_autoreload():
+    """
+    Private function to setup autoreload in an IPython environment.
+    """
+    try:
+        from IPython import get_ipython
+        ipython = get_ipython()
+        if ipython:
+            ipython.run_line_magic('load_ext', 'autoreload')
+            ipython.run_line_magic('autoreload', '2')
+    except ImportError:
+        pass  # Fails silently if not in an IPython environment
+
+# Automatically set up autoreload when this module is imported
+_setup_autoreload()
+
+import os
+import pickle as pkl
+
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+
+from mitools import clustering as clustering
+from mitools import etl, jupyter_utils, nlp, utils
+from mitools import notebooks as nb
+from mitools import pandas as pdf
+from mitools.country_converter import cc
+from mitools.utils import iprint
