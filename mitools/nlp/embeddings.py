487ac95bc13cde13fd2307d28f5d62042e292fa6
montanon <montagna.p.92@hotmail.com>
2023-11-15 18:42:50 +0900

    added gpu default support

--- mitools/nlp/embeddings.py
+++ mitools/nlp/embeddings.py
@@ -2,9 +2,9 @@ import warnings
 from ast import literal_eval
 from typing import Callable, Iterable, List, Optional, Union
 
+import torch
 from nltk.tokenize.api import StringTokenizer
-from numba.core.errors import (NumbaDeprecationWarning,
-                               NumbaPendingDeprecationWarning)
+from numba.core.errors import NumbaDeprecationWarning, NumbaPendingDeprecationWarning
 from numpy import float64, ndarray
 from pandas import DataFrame, Series
 from tqdm import tqdm
@@ -22,8 +22,9 @@ MAX_BATCH_SIZE = 4
 
 def huggingface_specter_embed_texts(texts: Union[List[str],str], batch_size: Optional[int]=MAX_BATCH_SIZE, 
                                     n_threads: Optional[int]=1) -> List[ndarray]:
+    device = 'mps' if torch.backends.mps.is_available() else 'cpu'
     tokenizer = AutoTokenizer.from_pretrained('allenai/specter')
-    model = AutoModel.from_pretrained('allenai/specter')
+    model = AutoModel.from_pretrained('allenai/specter').to(device)
     
     if n_threads > 1:
         parallel_function = parallel(n_threads, batch_size)(huggingface_specter_embed_texts_parallel)
@@ -39,8 +40,10 @@ def huggingface_specter_embed_texts_parallel(chunk: Iterable, tokenizer: StringT
 
 def huggingface_specter_embed_chunk(chunk: Iterable, tokenizer: StringTokenizer, model: AutoModel) -> List[ndarray]:
     inputs = tokenizer(chunk, padding=True, truncation=True, return_tensors="pt", max_length=512)
+    device = 'mps' if torch.backends.mps.is_available() else 'cpu'
+    inputs = inputs.to(device)
     result = model(**inputs)
-    return result.last_hidden_state[:, 0, :].detach().numpy().tolist()
+    return result.last_hidden_state[:, 0, :].detach().to('cpu').numpy().tolist()
 
 def semantic_scholar_specter_embed_texts(texts: Union[List[str],str], batch_size: Optional[int]=MAX_BATCH_SIZE
                                          ) -> List[ndarray]:
