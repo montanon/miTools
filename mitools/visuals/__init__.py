c069dd1d10b3c41714ab3743eae2189b67f0d573
montanon <montagna.p.92@hotmail.com>
2024-01-27 16:01:47 +0900

    ref: reorganized axes in visuals repos

---
+++ mitools/visuals/__init__.py
@@ -0,0 +1,9 @@
+from .axes_functions import (
+    adjust_axes_labels,
+    adjust_axes_lims,
+    adjust_text_axes_limits,
+    get_axes_limits,
+    is_ax_empty,
+    is_axes_empty,
+    set_axes_limits,
+)
