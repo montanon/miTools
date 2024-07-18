23f5481edf97a47da754344a3592adbc59adea44
montanon <montagna.p.92@hotmail.com>
2024-07-18 16:56:47 +0900

    fix: formatting

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -18,7 +18,7 @@ from treelib import Tree
 from mitools.utils import (
     BitArray,
     add_significance,
-    auto_adjust_columns_width,
+    auto_adjust_sheet_columns_width,
     build_dir_tree,
     can_convert_to,
     check_symmetrical_matrix,
@@ -79,124 +79,132 @@ class TestIterableChunks(unittest.TestCase):
         with self.assertRaises(TypeError):
             list(iterable_chunks(iterable, chunk_size))
 
+
 class TestStrIsNumber(unittest.TestCase):
     def test_integer_string(self):
-        self.assertTrue(str_is_number('123'))
+        self.assertTrue(str_is_number("123"))
 
     def test_float_string(self):
-        self.assertTrue(str_is_number('123.456'))
+        self.assertTrue(str_is_number("123.456"))
 
     def test_negative_integer_string(self):
-        self.assertTrue(str_is_number('-123'))
+        self.assertTrue(str_is_number("-123"))
 
     def test_negative_float_string(self):
-        self.assertTrue(str_is_number('-123.456'))
+        self.assertTrue(str_is_number("-123.456"))
 
     def test_non_numeric_string(self):
-        self.assertFalse(str_is_number('abc'))
+        self.assertFalse(str_is_number("abc"))
 
     def test_empty_string(self):
-        self.assertFalse(str_is_number(''))
+        self.assertFalse(str_is_number(""))
+
 
 class TestGetNumbersFromStr(unittest.TestCase):
     def test_integer_string(self):
-        string = 'abc 123 def 456'
+        string = "abc 123 def 456"
         self.assertEqual(get_numbers_from_str(string), [123.0, 456.0])
 
     def test_float_string(self):
-        string = 'abc 123.456 def 789.012'
+        string = "abc 123.456 def 789.012"
         self.assertEqual(get_numbers_from_str(string), [123.456, 789.012])
 
     def test_negative_number_string(self):
-        string = 'abc -123 def -456'
+        string = "abc -123 def -456"
         self.assertEqual(get_numbers_from_str(string), [-123.0, -456.0])
 
     def test_non_numeric_string(self):
-        string = 'abc def'
+        string = "abc def"
         self.assertEqual(get_numbers_from_str(string), [])
 
     def test_empty_string(self):
-        string = ''
+        string = ""
         self.assertEqual(get_numbers_from_str(string), [])
 
     def test_indexed_return(self):
-        string = 'abc 123 def 456'
+        string = "abc 123 def 456"
         self.assertEqual(get_numbers_from_str(string, 1), 456.0)
+
 
 class TestRemoveMultipleSpaces(unittest.TestCase):
     def test_multiple_spaces(self):
-        string = 'abc   def   ghi'
-        self.assertEqual(remove_multiple_spaces(string), 'abc def ghi')
+        string = "abc   def   ghi"
+        self.assertEqual(remove_multiple_spaces(string), "abc def ghi")
 
     def test_tabs(self):
-        string = 'abc\t\t\tdef\t\t\tghi'
-        self.assertEqual(remove_multiple_spaces(string), 'abc def ghi')
+        string = "abc\t\t\tdef\t\t\tghi"
+        self.assertEqual(remove_multiple_spaces(string), "abc def ghi")
 
     def test_newlines(self):
-        string = 'abc\n\ndef\n\nghi'
-        self.assertEqual(remove_multiple_spaces(string), 'abc def ghi')
+        string = "abc\n\ndef\n\nghi"
+        self.assertEqual(remove_multiple_spaces(string), "abc def ghi")
 
     def test_mixed_whitespace(self):
-        string = 'abc \t \n def \t \n ghi'
-        self.assertEqual(remove_multiple_spaces(string), 'abc def ghi')
+        string = "abc \t \n def \t \n ghi"
+        self.assertEqual(remove_multiple_spaces(string), "abc def ghi")
 
     def test_no_extra_spaces(self):
-        string = 'abc def ghi'
-        self.assertEqual(remove_multiple_spaces(string), 'abc def ghi')
+        string = "abc def ghi"
+        self.assertEqual(remove_multiple_spaces(string), "abc def ghi")
+
 
 class TestFindStrLineNumberInText(unittest.TestCase):
     def test_substring_at_start(self):
-        text = 'abc\ndef\nghi'
-        substring = 'abc'
+        text = "abc\ndef\nghi"
+        substring = "abc"
         self.assertEqual(find_str_line_number_in_text(text, substring), 0)
 
     def test_substring_in_middle(self):
-        text = 'abc\ndef\nghi'
-        substring = 'def'
+        text = "abc\ndef\nghi"
+        substring = "def"
         self.assertEqual(find_str_line_number_in_text(text, substring), 1)
 
     def test_substring_at_end(self):
-        text = 'abc\ndef\nghi'
-        substring = 'ghi'
+        text = "abc\ndef\nghi"
+        substring = "ghi"
         self.assertEqual(find_str_line_number_in_text(text, substring), 2)
 
     def test_substring_not_found(self):
-        text = 'abc\ndef\nghi'
-        substring = 'jkl'
+        text = "abc\ndef\nghi"
+        substring = "jkl"
         self.assertEqual(find_str_line_number_in_text(text, substring), None)
 
+
 class TestReadTextFile(unittest.TestCase):
-    @patch('builtins.open', new_callable=mock_open, read_data='abc\ndef\nghi')
+    @patch("builtins.open", new_callable=mock_open, read_data="abc\ndef\nghi")
     def test_read_text_file(self, mock_file):
-        text_path = 'dummy_path'
+        text_path = "dummy_path"
         result = read_text_file(text_path)
-        self.assertEqual(result, 'abc\ndef\nghi')
-        mock_file.assert_called_once_with(text_path, 'r')
+        self.assertEqual(result, "abc\ndef\nghi")
+        mock_file.assert_called_once_with(text_path, "r")
+
 
 class TestDictFromKwargs(unittest.TestCase):
     def test_no_arguments(self):
         self.assertEqual(dict_from_kwargs(), {})
 
     def test_one_argument(self):
-        self.assertEqual(dict_from_kwargs(a=1), {'a': 1})
+        self.assertEqual(dict_from_kwargs(a=1), {"a": 1})
 
     def test_multiple_arguments(self):
-        self.assertEqual(dict_from_kwargs(a=1, b=2, c=3), {'a': 1, 'b': 2, 'c': 3})
+        self.assertEqual(dict_from_kwargs(a=1, b=2, c=3), {"a": 1, "b": 2, "c": 3})
+
 
 class TestLcsSimilarity(unittest.TestCase):
     def test_identical_strings(self):
-        self.assertEqual(lcs_similarity('abc', 'abc'), 1.0)
+        self.assertEqual(lcs_similarity("abc", "abc"), 1.0)
 
     def test_different_strings(self):
-        self.assertEqual(lcs_similarity('abc', 'def'), 0.0)
+        self.assertEqual(lcs_similarity("abc", "def"), 0.0)
 
     def test_common_subsequence(self):
-        self.assertEqual(lcs_similarity('abc', 'adc'), 2/3)
+        self.assertEqual(lcs_similarity("abc", "adc"), 2 / 3)
 
     def test_empty_string(self):
-        self.assertEqual(lcs_similarity('abc', ''), 0.0)
-        self.assertEqual(lcs_similarity('', 'abc'), 0.0)
-        self.assertEqual(lcs_similarity('', ''), 0.0)
+        self.assertEqual(lcs_similarity("abc", ""), 0.0)
+        self.assertEqual(lcs_similarity("", "abc"), 0.0)
+        self.assertEqual(lcs_similarity("", ""), 0.0)
+
 
 class TestFuzzStringInString(unittest.TestCase):
     def test_fuzz_string_in_string_exact_match(self):
@@ -219,6 +227,7 @@ class TestFuzzStringInString(unittest.TestCase):
         dst_string = "Hello"
         self.assertTrue(fuzz_string_in_string(src_string, dst_string, 50))
 
+
 class TestFuzzRatio(unittest.TestCase):
     def test_fuzz_ratio_exact_match(self):
         src_string = "Hello World"
@@ -228,12 +237,19 @@ class TestFuzzRatio(unittest.TestCase):
     def test_fuzz_ratio_no_match(self):
         src_string = "Hello World"
         dst_string = "Goodbye World"
-        self.assertEqual(fuzz_ratio(src_string, dst_string), fuzz.partial_ratio(src_string, dst_string))
+        self.assertEqual(
+            fuzz_ratio(src_string, dst_string),
+            fuzz.partial_ratio(src_string, dst_string),
+        )
 
     def test_fuzz_ratio_partial_match(self):
         src_string = "Hello World"
         dst_string = "Hello"
-        self.assertEqual(fuzz_ratio(src_string, dst_string), fuzz.partial_ratio(src_string, dst_string))
+        self.assertEqual(
+            fuzz_ratio(src_string, dst_string),
+            fuzz.partial_ratio(src_string, dst_string),
+        )
+
 
 class TestReplacePrefix(unittest.TestCase):
     def test_replace_prefix(self):
@@ -254,10 +270,13 @@ class TestReplacePrefix(unittest.TestCase):
         replacement = "Goodbye"
         self.assertEqual(replace_prefix(string, prefix, replacement), "")
 
+
 class TestSplitStrings(unittest.TestCase):
     def test_split_strings(self):
         str_list = ["HelloWorld", "GoodByeWorld"]
-        self.assertEqual(split_strings(str_list), ["Hello", "World", "Good", "Bye", "World"])
+        self.assertEqual(
+            split_strings(str_list), ["Hello", "World", "Good", "Bye", "World"]
+        )
 
     def test_split_strings_no_capital_letters(self):
         str_list = ["hello", "world"]
@@ -266,6 +285,7 @@ class TestSplitStrings(unittest.TestCase):
     def test_split_strings_empty_list(self):
         str_list = []
         self.assertEqual(split_strings(str_list), [])
+
 
 class TestAddSignificance(unittest.TestCase):
     def test_add_significance_very_significant(self):
@@ -284,11 +304,12 @@ class TestAddSignificance(unittest.TestCase):
         row = Series(["Test (0.2)"])
         self.assertEqual(row.apply(add_significance)[0], "Test (0.2)")
 
+
 class TestRemoveDataframeDuplicates(unittest.TestCase):
     def setUp(self):
-        self.df1 = DataFrame({'A': [1, 2, 3], 'B': [4, 5, 6]})
-        self.df2 = DataFrame({'A': [1, 2, 3], 'B': [4, 5, 6]})
-        self.df3 = DataFrame({'A': [7, 8, 9], 'B': [10, 11, 12]})
+        self.df1 = DataFrame({"A": [1, 2, 3], "B": [4, 5, 6]})
+        self.df2 = DataFrame({"A": [1, 2, 3], "B": [4, 5, 6]})
+        self.df3 = DataFrame({"A": [7, 8, 9], "B": [10, 11, 12]})
 
     def test_remove_dataframe_duplicates(self):
         dfs = [self.df1, self.df2, self.df3]
@@ -309,6 +330,7 @@ class TestRemoveDataframeDuplicates(unittest.TestCase):
         unique_dfs = remove_dataframe_duplicates(dfs)
         self.assertEqual(len(unique_dfs), 1)
         self.assertTrue(unique_dfs[0].equals(self.df1))
+
 
 class TestCanConvertTo(unittest.TestCase):
     def test_can_convert_to_int_from_int(self):
@@ -344,12 +366,13 @@ class TestCanConvertTo(unittest.TestCase):
         self.assertTrue(can_convert_to(items, bool))
 
     def test_can_convert_to_int_from_int_fail(self):
-        items = [1, 2, 3, 'fail']
+        items = [1, 2, 3, "fail"]
         self.assertFalse(can_convert_to(items, int))
-        
+
     def test_can_convert_to_float_from_float_fail(self):
-        items = [1.0, 2.0, 3.0, 'fail']
+        items = [1.0, 2.0, 3.0, "fail"]
         self.assertFalse(can_convert_to(items, float))
+
 
 class TestInvertDict(unittest.TestCase):
     def test_invert_dict(self):
@@ -367,28 +390,30 @@ class TestInvertDict(unittest.TestCase):
         inverted = {1: "b", 2: "c"}
         self.assertEqual(invert_dict(dictionary), inverted)
 
+
 class TestIPrint(unittest.TestCase):
-    @patch('builtins.print')
+    @patch("builtins.print")
     def test_iprint_string(self, mock_print):
         iprint("Hello World")
         mock_print.assert_called_with("Hello World")
 
-    @patch('builtins.print')
+    @patch("builtins.print")
     def test_iprint_list(self, mock_print):
         iprint(["Hello", "World"])
         calls = [call("Hello"), call("World")]
         mock_print.assert_has_calls(calls, any_order=True)
 
-    @patch('builtins.print')
+    @patch("builtins.print")
     def test_iprint_splitter(self, mock_print):
-        iprint("Hello World", splitter='-')
-        calls = [call('-' * 40), call("Hello World"), call('-' * 40)]
+        iprint("Hello World", splitter="-")
+        calls = [call("-" * 40), call("Hello World"), call("-" * 40)]
         mock_print.assert_has_calls(calls, any_order=True)
 
-    @patch('builtins.print')
+    @patch("builtins.print")
     def test_iprint_color(self, mock_print):
-        iprint("Hello World", c='red')
+        iprint("Hello World", c="red")
         mock_print.assert_called_with("\033[91mHello World\033[0m")
+
 
 class TestCheckSymmetricalMatrix(unittest.TestCase):
     def test_check_symmetrical_matrix_symmetrical(self):
@@ -407,8 +432,8 @@ class TestCheckSymmetricalMatrix(unittest.TestCase):
         a = np.array([[1, 2, 3], [2, 1, 4.1], [3, 4, 1]])
         self.assertFalse(check_symmetrical_matrix(a, rtol=1e-04))
 
-class TestRemoveChars(unittest.TestCase):
 
+class TestRemoveChars(unittest.TestCase):
     def test_basic_removal(self):
         self.assertEqual(remove_chars("Hello, World!", "lo"), "He, Wrd!")
 
@@ -427,6 +452,7 @@ class TestRemoveChars(unittest.TestCase):
     def test_special_characters(self):
         self.assertEqual(remove_chars("H@#llo, W$rld!", "@#$"), "Hllo, Wrld!")
 
+
 class TestStorePklObject(unittest.TestCase):
     def setUp(self):
         self.test_object = {"key": "value"}
@@ -438,9 +464,10 @@ class TestStorePklObject(unittest.TestCase):
 
     def test_store_pkl_object(self):
         store_pkl_object(self.test_object, self.filename)
-        with open(self.filename, 'rb') as input_file:
+        with open(self.filename, "rb") as input_file:
             loaded_object = pickle.load(input_file)
         self.assertEqual(loaded_object, self.test_object)
+
 
 class TestLoadPklObject(unittest.TestCase):
     def setUp(self):
@@ -455,6 +482,7 @@ class TestLoadPklObject(unittest.TestCase):
     def test_load_pkl_object(self):
         loaded_object = load_pkl_object(self.filename)
         self.assertEqual(loaded_object, self.test_object)
+
 
 class TestUnpackListOfLists(unittest.TestCase):
     def test_unpack_list_of_lists(self):
@@ -472,8 +500,8 @@ class TestUnpackListOfLists(unittest.TestCase):
         unpacked = [1, 2, 3]
         self.assertEqual(unpack_list_of_lists(list_of_lists), unpacked)
 
-class TestBitArray(unittest.TestCase):
 
+class TestBitArray(unittest.TestCase):
     def test_zeros_initialization(self):
         size = 16
         bit_array = BitArray.zeros(size)
@@ -504,14 +532,19 @@ class TestBitArray(unittest.TestCase):
         expected_repr = "BitArray([0, 0, 0, 0, 0, 0, 0, 0])"
         self.assertEqual(repr(bit_array), expected_repr)
 
+
 class TestStretchString(unittest.TestCase):
     def test_normal_case(self):
-        self.assertEqual(stretch_string("This is a sample string for testing purposes", 10),
-                         "This is a\nsample\nstring for\ntesting\npurposes")
+        self.assertEqual(
+            stretch_string("This is a sample string for testing purposes", 10),
+            "This is a\nsample\nstring for\ntesting\npurposes",
+        )
 
     def test_no_spaces(self):
-        self.assertEqual(stretch_string("LongStringWithNoSpaces", 5),
-                         "LongS\ntring\nWithN\noSpac\nes")
+        self.assertEqual(
+            stretch_string("LongStringWithNoSpaces", 5),
+            "LongS\ntring\nWithN\noSpac\nes",
+        )
 
     def test_edge_cases(self):
         self.assertEqual(stretch_string("", 10), "")
@@ -519,35 +552,40 @@ class TestStretchString(unittest.TestCase):
         self.assertEqual(stretch_string("ExactlyTen", 10), "ExactlyTen")
 
     def test_whitespace_handling(self):
-        self.assertEqual(stretch_string("  This   string has  weird spacing ", 10),
-                         "This\nstring has\nweird\nspacing")
+        self.assertEqual(
+            stretch_string("  This   string has  weird spacing ", 10),
+            "This\nstring has\nweird\nspacing",
+        )
 
     def test_long_word(self):
-        self.assertEqual(stretch_string("Supercalifragilisticexpialidocious", 10),
-                         "Supercalif\nragilistic\nexpialidoc\nious")
+        self.assertEqual(
+            stretch_string("Supercalifragilisticexpialidocious", 10),
+            "Supercalif\nragilistic\nexpialidoc\nious",
+        )
+
 
 class TestAutoAdjustColumnsWidth(unittest.TestCase):
-    @patch('openpyxl.utils.get_column_letter', return_value='A')
+    @patch("openpyxl.utils.get_column_letter", return_value="A")
     def test_auto_adjust_columns_width(self, mock_get_column_letter):
         # Create a mock worksheet with some columns
         wb = Workbook()
         ws = wb.active
-        ws.append(['Hello', 'World'])
-        ws.append(['Longer string here', 'Another string'])
+        ws.append(["Hello", "World"])
+        ws.append(["Longer string here", "Another string"])
         # Call the function to test
-        auto_adjust_columns_width(ws)
+        auto_adjust_sheet_columns_width(ws)
         # Check that the width of the columns has been adjusted
-        self.assertEqual(ws.column_dimensions['A'].width, 15)
-        self.assertEqual(ws.column_dimensions['B'].width, 13)
+        self.assertEqual(ws.column_dimensions["A"].width, 15)
+        self.assertEqual(ws.column_dimensions["B"].width, 13)
+
 
 class TestDisplayEnvVariables(unittest.TestCase):
-
     def setUp(self):
         self.env_vars = [
-            ('small_int', 1),
-            ('large_list', list(range(10000))),
-            ('string', 'hello world'),
-            ('large_dict', {i: i for i in range(1000)})
+            ("small_int", 1),
+            ("large_list", list(range(10000))),
+            ("string", "hello world"),
+            ("large_dict", {i: i for i in range(1000)}),
         ]
 
     def test_no_large_variables(self):
@@ -559,7 +597,7 @@ class TestDisplayEnvVariables(unittest.TestCase):
         threshold_mb = 0
         df = display_env_variables(self.env_vars, threshold_mb)
         self.assertFalse(df.empty)
-        self.assertTrue(all(df['Size (MB)'] > threshold_mb))
+        self.assertTrue(all(df["Size (MB)"] > threshold_mb))
 
     def test_edge_cases(self):
         # Empty env_vars
@@ -568,12 +606,13 @@ class TestDisplayEnvVariables(unittest.TestCase):
         # Extremely high threshold
         df_high_threshold = display_env_variables(self.env_vars, 1000000)
         self.assertTrue(df_high_threshold.empty)
-        
+
     def test_different_data_types(self):
         threshold_mb = 0
         df = display_env_variables(self.env_vars, threshold_mb)
-        self.assertIn('large_list', df['Variable'].values)
-        self.assertIn('large_dict', df['Variable'].values)
+        self.assertIn("large_list", df["Variable"].values)
+        self.assertIn("large_dict", df["Variable"].values)
+
 
 class TestPrettyDictStr(unittest.TestCase):
     def test_pretty_dict_str(self):
@@ -588,12 +627,13 @@ class TestPrettyDictStr(unittest.TestCase):
         expected_str = json.dumps(dictionary, indent=4, sort_keys=True)
         self.assertEqual(pretty_str, expected_str)
 
+
 class TestBuildDirTree(unittest.TestCase):
     def setUp(self):
         self.test_dir = Path(tempfile.mkdtemp())
-        self.sub_dir = self.test_dir / 'sub_dir'
+        self.sub_dir = self.test_dir / "sub_dir"
         self.sub_dir.mkdir()
-        (self.sub_dir / 'file.txt').touch()
+        (self.sub_dir / "file.txt").touch()
         self.tree = Tree()
 
     def tearDown(self):
@@ -602,8 +642,9 @@ class TestBuildDirTree(unittest.TestCase):
     def test_build_dir_tree(self):
         tree = build_dir_tree(self.test_dir, self.tree)
         self.assertEqual(len(tree.all_nodes()), len(self.tree.all_nodes()))
-        self.assertTrue(any(node.tag == 'sub_dir' for node in tree.all_nodes()))
-        self.assertTrue(any(node.tag == 'file.txt' for node in tree.all_nodes()))
+        self.assertTrue(any(node.tag == "sub_dir" for node in tree.all_nodes()))
+        self.assertTrue(any(node.tag == "file.txt" for node in tree.all_nodes()))
+
 
 class TestCleanStr(unittest.TestCase):
     def test_clean_str_no_pattern(self):
@@ -625,5 +666,5 @@ class TestCleanStr(unittest.TestCase):
         self.assertEqual(result, "Hello; World!")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
