ddbcb4b9a94bf30710fa2a416fab1b6e09310a23
montanon <montagna.p.92@hotmail.com>
2024-10-08 14:24:55 -0300

    feat: added empty_df error

--- mitools/clustering/clustering_evaluations.py
+++ mitools/clustering/clustering_evaluations.py
@@ -13,7 +13,7 @@ CLUSTER_COL_NOT_IN_INDEX_ERROR = (
     "DataFrame provided does not have the {cluster_col} index level!"
 )
 SINGLE_GROUP_DF_ERROR = "DataFrame provided has a single group!"
-EMPTY_DF_ERROR = "DataFrame provided is empty!"
+EMPTY_DATA_ERROR = "Input DataFrame cannot be empty."
 
 
 def get_clusters_centroids(
@@ -23,6 +23,8 @@ def get_clusters_centroids(
 ) -> DataFrame:
     if cluster_level not in data.index.names:
         raise KeyError(f"{CLUSTER_COL_NOT_IN_INDEX_ERROR}")
+    if data.empty:
+        raise ArgumentStructureError(EMPTY_DATA_ERROR)
     if data.index.get_level_values(cluster_level).nunique() == 1:
         raise ArgumentStructureError(SINGLE_GROUP_DF_ERROR)
     clf = NearestCentroid(metric=metric)
