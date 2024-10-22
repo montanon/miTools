98bd7995d5724bd236db73dcf7bee4206fb08f20
montanon <montagna.p.92@hotmail.com>
2024-10-22 11:09:52 -0300

    feat: refactored folder_is_subfolder

--- mitools/files/file_handlers.py
+++ mitools/files/file_handlers.py
@@ -1,11 +1,23 @@
-import pathlib
+from os import PathLike
 from pathlib import Path
 from typing import List, Union
 
+from mitools.exceptions import ArgumentValueError
 
-def folder_is_subfolder(root_folder: Path, folder_to_check: Path) -> bool:
-    root_folder = root_folder.resolve()
-    folder_to_check = folder_to_check.resolve()
+
+def folder_is_subfolder(root_folder: PathLike, folder_to_check: PathLike) -> bool:
+    root_folder = Path(root_folder)
+    folder_to_check = Path(folder_to_check)
+    try:
+        root_folder = root_folder.resolve(strict=False)
+    except Exception as e:
+        raise ArgumentValueError(f"Invalid {root_folder} path provided: {e}")
+    try:
+        folder_to_check = folder_to_check.resolve(strict=False)
+    except Exception as e:
+        raise ArgumentValueError(f"Invalid {folder_to_check} path provided: {e}")
+    if root_folder == folder_to_check:
+        return False
     return root_folder in folder_to_check.parents
 
 
