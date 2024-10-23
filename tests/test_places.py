93ac772196ababfb61aabce664f7cc9537dd3768
montanon <montagna.p.92@hotmail.com>
2024-10-23 10:23:12 -0300

    feat: adjusted imports of places submodule

--- tests/test_places.py
+++ tests/test_places.py
@@ -4,25 +4,60 @@ from unittest.mock import MagicMock, patch
 import geopandas as gpd
 from shapely import Point
 
-from mitools.google import (
+from mitools.google.places import (
+    AccessibilityOptions,
+    AddressComponent,
+    CircleCenterInsidePolygon,
+    CircleInsidePolygon,
+    CircleIntersectsPolygon,
+    CityGeojson,
+    ConditionProtocol,
     DummyResponse,
+    NearbySearchRequest,
+    NewNearbySearchRequest,
+    NewPlace,
+    Place,
+    Viewport,
+    ViewportCoordinate,
+    calculate_degree_steps,
+    create_dummy_place,
     create_dummy_response,
+    create_subsampled_circles,
+    generate_unique_place_id,
+    get_circles_search,
+    get_response_places,
+    get_saturated_area,
+    intersection_condition_factory,
     meters_to_degree,
     nearby_search_request,
+    places_search_step,
+    polygon_plot_with_circles_and_points,
+    polygon_plot_with_points,
+    polygon_plot_with_sampling_circles,
+    polygons_folium_map,
+    polygons_folium_map_with_pois,
+    process_circles,
+    read_or_initialize_places,
+    sample_polygon_with_circles,
+    sample_polygons_with_circles,
+    search_and_update_places,
+    search_places_in_polygon,
+    update_progress_and_save,
 )
 
 
 class TestDummyResponse(unittest.TestCase):
-
     latitude = 35.5
     longitude = 120.7
     radius = 200
-    circle = gpd.GeoSeries([Point(latitude, longitude).buffer(meters_to_degree(radius, latitude))])
+    circle = gpd.GeoSeries(
+        [Point(latitude, longitude).buffer(meters_to_degree(radius, latitude))]
+    )
 
     query = nearby_search_request(circle, radius)
 
     print(query)
 
-    
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
