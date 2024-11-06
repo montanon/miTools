31e12c3af9674076df958c33b7116cc22a704f07
montanon <montagna.p.92@hotmail.com>
2024-11-06 17:57:43 -0300

    feat: added validate_args_types

--- mitools/utils/decorators.py
+++ mitools/utils/decorators.py
@@ -5,6 +5,8 @@ from typing import Callable, Iterable
 
 from tqdm import tqdm
 
+from mitools.exceptions import ArgumentTypeError, ArgumentValueError
+
 from .helper_functions import iterable_chunks
 
 
@@ -12,16 +14,21 @@ def parallel(n_threads: int, chunk_size: int):
     def decorator(func: Callable):
         @wraps(func)
         def wrapper(iterable: Iterable, *args, **kwargs):
-            print('Parallelizing!')
+            print("Parallelizing!")
             chunks = list(iterable_chunks(iterable, chunk_size))
             results = []
             with Pool(processes=n_threads) as pool:
-                async_results = [pool.apply_async(func, (chunk, *args)) for chunk in chunks]
+                async_results = [
+                    pool.apply_async(func, (chunk, *args)) for chunk in chunks
+                ]
                 for async_result in tqdm(async_results, total=len(chunks)):
                     results.append(async_result.get())
             return [item for sublist in results for item in sublist]
+
         return wrapper
+
     return decorator
+
 
 def suppress_user_warning(func: Callable):
     @wraps(func)
@@ -29,4 +36,36 @@ def suppress_user_warning(func: Callable):
         with warnings.catch_warnings():
             warnings.simplefilter("ignore", category=UserWarning)
             return func(*args, **kwargs)
+
     return wrapper
+
+
+def validate_args_types(**type_hints):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            for name, expected_type in type_hints.items():
+                if name in kwargs:
+                    value = kwargs[name]
+                else:
+                    try:
+                        arg_names = func.__code__.co_varnames[
+                            : func.__code__.co_argcount
+                        ]
+                        arg_index = arg_names.index(name)
+                        value = args[arg_index]
+                    except (ValueError, IndexError):
+                        raise ArgumentValueError(
+                            f"Argument '{name}' not found in function signature"
+                        )
+
+                if not isinstance(value, expected_type):
+                    raise ArgumentTypeError(
+                        f"Argument '{name}' must be of type {expected_type.__name__}"
+                    )
+
+            return func(*args, **kwargs)
+
+        return wrapper
+
+    return decorator
