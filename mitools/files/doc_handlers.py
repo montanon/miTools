32700790e4f1c5476ed2c6e3b7fe1264b7fc5fd6
montanon <montagna.p.92@hotmail.com>
2024-11-06 17:16:10 -0300

    feat: added convert_docx_to_pdf and batch_convert_docx_to_pdf

--- mitools/files/doc_handlers.py
+++ mitools/files/doc_handlers.py
@@ -1,26 +1,39 @@
+from os import PathLike
 from pathlib import Path
 from typing import List, Union
 
+import pypandoc
 from docx import Document
 
+from mitools.exceptions import ArgumentValueError
 
-def read_docx_file(file_path: Union[str, Path]) -> List[str]:
+
+def read_docx_file(file_path: Union[str, Path], indent: str = "-") -> List[str]:
     document = Document(file_path)
-    text = []
+    paragraphs = []
     previous_style = None
-    indent = "- "
+    previous_punctuation = False
+    n_tabs = 0
     for n, para in enumerate(document.paragraphs):
         if para.text:
+            text = para.text.strip()
             formatted_text = ""
             if previous_style is None:
                 previous_style = para.style.name
-            if para.style.name != previous_style:
+            if (
+                para.style.name != previous_style
+                or text.endswith(":")
+                and not previous_style.find("Heading")
+            ):
                 if previous_style == "Normal" and para.style.name == "List Paragraph":
-                    indent = "\t- "
+                    n_tabs += 1
                 elif previous_style == "List Paragraph" and para.style.name == "Normal":
-                    indent = "- "
+                    n_tabs -= 1
+                if text.endswith(":"):
+                    n_tabs += 1
             previous_style = para.style.name
             style_name = para.style.name
+            previous_punctuation = text.endswith(":")
             if "Heading" in style_name:
                 formatted_text += f"{'#'*int(style_name[-1])} "
             highlighted = False
@@ -35,5 +48,41 @@ def read_docx_file(file_path: Union[str, Path]) -> List[str]:
                 formatted_text += run_text
             if highlighted:
                 formatted_text = f"<highlight>{formatted_text}<\highlight>"
-            text.append(f"{indent if n != 0 else ''}{formatted_text}")
-    return text
+            indentation = "\t" * n_tabs + indent
+            paragraphs.append(
+                f"{indentation if n_tabs != 0 else ''}{formatted_text[:20]} -- ({para.style.name}, {previous_style}, {previous_punctuation})"
+            )
+    return paragraphs
+
+
+def convert_docx_to_pdf(
+    file_path: PathLike,
+    output_path: PathLike,
+    exist_ok: bool = True,
+    overwrite: bool = True,
+) -> None:
+    if Path(output_path).exists() and not exist_ok and not overwrite:
+        raise ArgumentValueError(f"'{output_path}' already exists.")
+    elif Path(output_path).exists() and exist_ok and not overwrite:
+        return
+    output = pypandoc.convert_file(str(file_path), "pdf", outputfile=str(output_path))
+    assert output == "", f"Conversion failed: {output}"
+
+
+def batch_convert_docx_to_pdf(
+    directory: PathLike,
+    output_directory: PathLike = None,
+    exist_ok: bool = True,
+    overwrite: bool = True,
+) -> None:
+    for docx_file in directory.glob("*.docx"):
+        pdf_file = (
+            output_directory / docx_file.with_suffix(".pdf").name
+            if output_directory
+            else docx_file.with_suffix(".pdf")
+        )
+        convert_docx_to_pdf(docx_file, pdf_file, exist_ok, overwrite)
+
+
+if __name__ == "__main__":
+    pass
