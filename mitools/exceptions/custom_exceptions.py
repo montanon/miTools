600f66cb708b0990edaad151642e9a7f9b919004
montanon <montagna.p.92@hotmail.com>
2024-11-13 13:07:39 -0300

    feat: added some custom excpetions for dataframe structure validation

--- mitools/exceptions/custom_exceptions.py
+++ mitools/exceptions/custom_exceptions.py
@@ -1,21 +1,42 @@
-
-
 class ArgumentTypeError(TypeError):
     def __init__(self, message):
         self.message = message
         super().__init__(self.message)
+
 
 class ArgumentKeyError(KeyError):
     def __init__(self, message):
         self.message = message
         super().__init__(self.message)
 
+
 class ArgumentValueError(ValueError):
     def __init__(self, message):
         self.message = message
         super().__init__(self.message)
 
+
 class ArgumentStructureError(Exception):
     def __init__(self, message):
         self.message = message
         super().__init__(self.message)
+
+
+class ColumnValidationError(ArgumentValueError):
+    pass
+
+
+class IndexValidationError(ArgumentValueError):
+    pass
+
+
+class ColumnTypeError(ArgumentTypeError):
+    pass
+
+
+class IndexTypeError(ArgumentTypeError):
+    pass
+
+
+class ValuesTypeError(ArgumentTypeError):
+    pass
