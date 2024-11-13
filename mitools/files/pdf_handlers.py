5a676bfb8cb0818f7f9ca4c5350f319ccc1ec861
montanon <montagna.p.92@hotmail.com>
2024-11-13 09:48:27 -0300

    fix: fixed some miss-renaming

--- mitools/files/pdf_handlers.py
+++ mitools/files/pdf_handlers.py
@@ -76,7 +76,7 @@ def set_folder_pdfs_titles_as_filenames(
             print(f"Error processing '{file.name}': {e}")
 
 
-def pdf_to_markdown_file(pdf_path: PathLike, page_number: bool = False) -> str:
+def pdf_to_markdown(pdf_path: PathLike, page_number: bool = False) -> str:
     document = pymupdf.open(pdf_path)
     md_document = []
     for n in range(document.page_count):
@@ -90,7 +90,7 @@ def pdf_to_markdown_file(pdf_path: PathLike, page_number: bool = False) -> str:
 def pdf_to_markdown_file(
     pdf_path: PathLike, output_path: PathLike = None, page_number: bool = False
 ) -> str:
-    md_document = pdf_to_markdown_file(pdf_path=pdf_path, page_number=page_number)
+    md_document = pdf_to_markdown(pdf_path=pdf_path, page_number=page_number)
     if output_path is None:
         output_path = Path(pdf_path).with_suffix(".md")
     with open(output_path, "w") as f:
