cf3ebeff740c89ebbd9686c728509668ae1ed241
montanon <montagna.p.92@hotmail.com>
2024-11-15 23:58:48 -0300

    fix: rewrote imports from mitools

--- mitools/llms/__init__.py
+++ mitools/llms/__init__.py
@@ -1,2 +1,3 @@
-from .core_functions import *
-from .objects import *
+from mitools.llms.clients import OpenAIClient
+from mitools.llms.core_functions import *
+from mitools.llms.objects import LLMFactory, LLMModel, Prompt
