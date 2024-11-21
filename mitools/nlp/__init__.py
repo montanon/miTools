bd428686b838c2921c2a61e106d440502217d83a
montanon <montagna.p.92@hotmail.com>
2024-11-21 16:28:24 -0300

    fix: fixed embeddings imports

--- mitools/nlp/__init__.py
+++ mitools/nlp/__init__.py
@@ -1,7 +1,11 @@
 from .embeddings import (
-    embeddings_col_to_frame,
     huggingface_embed_texts,
+    plot_umap_connectivity,
+    plot_umap_diagnostic,
+    plot_umap_interactive,
+    plot_umap_points,
     specter_embed_texts,
+    tsne_embeddings,
     umap_embeddings,
 )
 from .keywords import *
