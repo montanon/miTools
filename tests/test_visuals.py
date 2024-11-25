8d8a04b1e656a93f491d048216d1dfb4537c9a19
montanon <montagna.p.92@hotmail.com>
2024-11-25 20:11:29 -0300

    feat: refactored adjust_ax_text_limits

--- tests/test_visuals.py
+++ tests/test_visuals.py
@@ -12,9 +12,9 @@ from matplotlib.text import Text
 from mitools.exceptions import ArgumentStructureError, ArgumentTypeError
 from mitools.visuals.axes_functions import (
     adjust_ax_labels_fontsize,
+    adjust_ax_text_limits,
     adjust_axes_labels_fontsize,
     adjust_axes_lims,
-    adjust_text_axes_limits,
     are_axes_empty,
     get_axes_limits,
     is_ax_empty,
