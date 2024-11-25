6efdd1c728472db9dfb94cb54d38fca924e9dc10
montanon <montagna.p.92@hotmail.com>
2024-11-25 19:13:13 -0300

    feat: added is_ax_empty tests

---
+++ tests/test_visuals.py
@@ -0,0 +1,84 @@
+import unittest
+from unittest import TestCase
+
+import numpy as np
+from matplotlib.axes import Axes
+from matplotlib.collections import PathCollection
+from matplotlib.figure import Figure
+from matplotlib.image import AxesImage
+from matplotlib.patches import Rectangle
+from matplotlib.text import Text
+
+from mitools.exceptions import ArgumentTypeError
+from mitools.visuals.axes_functions import (
+    adjust_axes_labels,
+    adjust_axes_lims,
+    adjust_text_axes_limits,
+    get_axes_limits,
+    is_ax_empty,
+    is_axes_empty,
+    set_axes_limits,
+)
+
+
+class TestIsAxEmpty(unittest.TestCase):
+    def setUp(self):
+        self.figure = Figure()
+        self.ax = self.figure.add_subplot(111)
+
+    def test_empty_axes(self):
+        self.assertTrue(is_ax_empty(self.ax))
+
+    def test_axes_with_lines(self):
+        self.ax.plot([0, 1], [0, 1], label="Test Line")
+        self.assertFalse(is_ax_empty(self.ax))
+
+    def test_axes_with_patches(self):
+        rect = Rectangle((0, 0), 1, 1)
+        self.ax.add_patch(rect)
+        self.assertFalse(is_ax_empty(self.ax))
+
+    def test_axes_with_collections(self):
+        x = np.random.rand(10)
+        y = np.random.rand(10)
+        self.ax.scatter(x, y)
+        self.assertFalse(is_ax_empty(self.ax))
+
+    def test_axes_with_texts(self):
+        self.ax.text(0.5, 0.5, "Test Text")
+        self.assertFalse(is_ax_empty(self.ax))
+
+    def test_axes_with_images(self):
+        data = np.random.rand(10, 10)
+        self.ax.imshow(data)
+        self.assertFalse(is_ax_empty(self.ax))
+
+    def test_axes_with_multiple_elements(self):
+        self.ax.plot([0, 1], [0, 1], label="Test Line")
+        self.ax.text(0.5, 0.5, "Test Text")
+        self.ax.scatter(np.random.rand(10), np.random.rand(10))
+        self.assertFalse(is_ax_empty(self.ax))
+
+    def test_axes_after_clearing(self):
+        self.ax.plot([0, 1], [0, 1], label="Test Line")
+        self.ax.cla()  # Clear the Axes
+        self.assertTrue(is_ax_empty(self.ax))
+
+    def test_invalid_input(self):
+        with self.assertRaises(ArgumentTypeError):
+            is_ax_empty("Not an Axes object")
+        with self.assertRaises(ArgumentTypeError):
+            is_ax_empty(None)
+
+    def test_axes_with_titles_or_labels(self):
+        self.ax.set_title("Test Title")
+        self.ax.set_xlabel("X-axis Label")
+        self.ax.set_ylabel("Y-axis Label")
+        self.assertTrue(is_ax_empty(self.ax))
+
+    def test_axes_with_spines_and_ticks(self):
+        self.assertTrue(is_ax_empty(self.ax))
+
+
+if __name__ == "__main__":
+    unittest.main()
