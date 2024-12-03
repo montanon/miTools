1d99b4251df8e1e7083a262177d5f723b60466d0
montanon <montagna.p.92@hotmail.com>
2024-12-03 13:04:36 -0300

    feat: standardize BarPlotter validation

--- mitools/visuals/plots/bar_plotter.py
+++ mitools/visuals/plots/bar_plotter.py
@@ -2,14 +2,11 @@ import re
 from typing import Any, Dict, Literal, Sequence, Union
 
 from matplotlib.axes import Axes
+from matplotlib.colors import Colormap
 from matplotlib.figure import Figure
-from numpy import integer, ndarray
-from pandas import Series
 
 from mitools.exceptions import (
-    ArgumentStructureError,
     ArgumentTypeError,
-    ArgumentValueError,
 )
 from mitools.visuals.plots.matplotlib_typing import (
     Cmap,
@@ -19,6 +16,18 @@ from mitools.visuals.plots.matplotlib_typing import (
     _colors,
 )
 from mitools.visuals.plots.plotter import Plotter
+from mitools.visuals.plots.validations import (
+    NUMERIC_TYPES,
+    SEQUENCE_TYPES,
+    is_sequence,
+    validate_length,
+    validate_non_negative,
+    validate_sequence_length,
+    validate_sequence_non_negative,
+    validate_sequence_type,
+    validate_type,
+    validate_value_in_options,
+)
 
 
 class BarPlotterException(Exception):
@@ -67,206 +76,206 @@ class BarPlotter(Plotter):
                 )
             self.color = color
             return self
-        if (
-            isinstance(color, (tuple, list, ndarray))
-            and len(color) in [3, 4]
-            and all(isinstance(c, (float, int, integer)) for c in color)
-        ):
+
+        if is_sequence(color):
+            if len(color) in [3, 4]:
+                validate_sequence_type(color, NUMERIC_TYPES, "color")
+                self.color = color
+                return self
+
+            validate_length(color, self.data_size, "color")
+            for c in color:
+                if isinstance(c, str):
+                    if c not in _colors and not re.match(
+                        r"^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{8})$", c
+                    ):
+                        raise ArgumentTypeError(
+                            f"'color' elements must be valid Matplotlib color strings or HEX codes, got '{c}'"
+                        )
+                elif is_sequence(c):
+                    validate_sequence_length(c, (3, 4), "color elements")
+                    validate_sequence_type(c, NUMERIC_TYPES, "color elements")
+                else:
+                    raise ArgumentTypeError(
+                        "color elements must be strings or RGB/RGBA values"
+                    )
             self.color = color
             return self
-        if isinstance(color, (list, tuple, ndarray, Series)):
-            if len(color) != self.data_size:
-                raise ArgumentStructureError(
-                    "color must be of the same length as x_data and y_data, "
-                    + f"len(color)={len(color)} != len(x_data)={self.data_size}."
-                )
-            if not all(
-                isinstance(c, str)
-                or (
-                    isinstance(c, (tuple, list, ndarray))
-                    and len(c) in [3, 4]
-                    and all(isinstance(x, (float, int, integer)) for x in c)
-                )
-                for c in color
-            ):
-                raise ArgumentTypeError(
-                    "All elements in color must be strings or RGB/RGBA values."
-                )
-            self.color = color
-            return self
+
         raise ArgumentTypeError(
             "color must be a string, RGB/RGBA values, or array-like of strings/RGB/RGBA values."
         )
 
     def set_width(self, width: Union[Sequence[float], float]):
-        if isinstance(width, (float, int)):
-            if width <= 0:
-                raise ArgumentValueError("width must be positive")
+        if isinstance(width, NUMERIC_TYPES):
+            validate_non_negative(width, "width")
             self.width = width
-        elif isinstance(width, (list, tuple, ndarray, Series)):
-            if len(width) != self.data_size:
-                raise ArgumentStructureError(
-                    "width must be of the same length as x_data and y_data"
-                )
-            if not all(isinstance(w, (float, int)) and w > 0 for w in width):
-                raise ArgumentTypeError("All width values must be positive numbers")
+        elif is_sequence(width):
+            validate_length(width, self.data_size, "width")
+            validate_sequence_type(width, NUMERIC_TYPES, "width")
+            validate_sequence_non_negative(width, "width")
             self.width = width
         else:
             raise ArgumentTypeError("width must be a number or sequence of numbers")
         return self
 
     def set_bottom(self, bottom: Union[Sequence[float], float]):
-        if isinstance(bottom, (float, int)):
+        if isinstance(bottom, NUMERIC_TYPES):
             self.bottom = bottom
-        elif isinstance(bottom, (list, tuple, ndarray, Series)):
-            if len(bottom) != self.data_size:
-                raise ArgumentStructureError(
-                    "bottom must be of the same length as x_data and y_data"
-                )
-            if not all(isinstance(b, (float, int)) for b in bottom):
-                raise ArgumentTypeError("All bottom values must be numbers")
+        elif is_sequence(bottom):
+            validate_length(bottom, self.data_size, "bottom")
+            validate_sequence_type(bottom, NUMERIC_TYPES, "bottom")
             self.bottom = bottom
         else:
             raise ArgumentTypeError("bottom must be a number or sequence of numbers")
         return self
 
     def set_align(self, align: Literal["center", "edge"]):
-        if align not in ["center", "edge"]:
-            raise ArgumentValueError("align must be either 'center' or 'edge'")
+        validate_value_in_options(align, ["center", "edge"], "align")
         self.align = align
         return self
 
     def set_edgecolor(self, edgecolor: EdgeColor):
-        if isinstance(edgecolor, str) or (
-            isinstance(edgecolor, (list, tuple))
-            and len(edgecolor) in [3, 4]
-            and all(isinstance(x, (int, float, integer)) for x in edgecolor)
-        ):
-            self.edgecolor = edgecolor
-        elif isinstance(edgecolor, (list, tuple, ndarray, Series)):
-            if len(edgecolor) != self.data_size:
-                raise ArgumentStructureError(
-                    "edgecolor must be of the same length as x_data and y_data"
+        if isinstance(edgecolor, str):
+            if edgecolor not in _colors and not re.match(
+                r"^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{8})$", edgecolor
+            ):
+                raise ArgumentTypeError(
+                    f"'edgecolor'='{edgecolor}' must be a valid Matplotlib color string or HEX code."
                 )
-            for ec in edgecolor:
-                if not (
-                    isinstance(ec, str)
-                    or (
-                        isinstance(ec, (list, tuple))
-                        and len(ec) in [3, 4]
-                        and all(isinstance(x, (int, float, integer)) for x in ec)
-                    )
-                ):
-                    raise ArgumentTypeError(
-                        "Each edgecolor must be a string or RGB/RGBA values."
-                    )
             self.edgecolor = edgecolor
+        elif isinstance(edgecolor, SEQUENCE_TYPES):
+            if len(edgecolor) == 3 or len(edgecolor) == 4:
+                validate_sequence_type(edgecolor, NUMERIC_TYPES, "edgecolor")
+                self.edgecolor = edgecolor
+            else:
+                validate_length(edgecolor, self.data_size, "edgecolor")
+                for ec in edgecolor:
+                    if isinstance(ec, str):
+                        if ec not in _colors and not re.match(
+                            r"^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{8})$", ec
+                        ):
+                            raise ArgumentTypeError(
+                                f"'edgecolor' elements must be valid Matplotlib color strings or HEX codes, got '{ec}'"
+                            )
+                    elif is_sequence(ec):
+                        validate_sequence_length(ec, (3, 4), "edgecolor elements")
+                        validate_sequence_type(ec, NUMERIC_TYPES, "edgecolor elements")
+                    else:
+                        raise ArgumentTypeError(
+                            "edgecolor elements must be strings or RGB/RGBA values"
+                        )
+                self.edgecolor = edgecolor
         else:
             raise ArgumentTypeError(
-                "edgecolor must be a color string, RGB/RGBA values, "
-                + "or an array-like of color strings/RGB/RGBA values."
+                "edgecolor must be a string, RGB/RGBA values, or array-like of strings/RGB/RGBA values."
             )
         return self
 
     def set_linewidth(self, linewidth: Union[Sequence[float], float]):
-        if isinstance(linewidth, (float, int)):
-            if linewidth < 0:
-                raise ArgumentValueError("linewidth must be non-negative")
+        if isinstance(linewidth, NUMERIC_TYPES):
+            validate_non_negative(linewidth, "linewidth")
             self.linewidth = linewidth
-        elif isinstance(linewidth, (list, tuple, ndarray, Series)):
-            if len(linewidth) != self.data_size:
-                raise ArgumentStructureError(
-                    "linewidth must be of the same length as x_data and y_data"
-                )
-            if not all(isinstance(lw, (float, int)) and lw >= 0 for lw in linewidth):
-                raise ArgumentTypeError(
-                    "All linewidth values must be non-negative numbers"
-                )
+        elif is_sequence(linewidth):
+            validate_length(linewidth, self.data_size, "linewidth")
+            validate_sequence_type(linewidth, NUMERIC_TYPES, "linewidth")
+            validate_sequence_non_negative(linewidth, "linewidth")
             self.linewidth = linewidth
         else:
             raise ArgumentTypeError("linewidth must be a number or sequence of numbers")
         return self
 
     def set_xerr(self, xerr: Union[Sequence[float], float]):
-        if isinstance(xerr, (float, int)):
+        if isinstance(xerr, NUMERIC_TYPES):
             self.xerr = xerr
-        elif isinstance(xerr, (list, tuple, ndarray, Series)):
-            if len(xerr) != self.data_size:
-                raise ArgumentStructureError(
-                    "xerr must be of the same length as x_data and y_data"
-                )
-            if not all(isinstance(x, (float, int)) for x in xerr):
-                raise ArgumentTypeError("All xerr values must be numbers")
+        elif is_sequence(xerr):
+            validate_length(xerr, self.data_size, "xerr")
+            validate_sequence_type(xerr, NUMERIC_TYPES, "xerr")
             self.xerr = xerr
         else:
             raise ArgumentTypeError("xerr must be a number or sequence of numbers")
         return self
 
     def set_yerr(self, yerr: Union[Sequence[float], float]):
-        if isinstance(yerr, (float, int)):
+        if isinstance(yerr, NUMERIC_TYPES):
             self.yerr = yerr
-        elif isinstance(yerr, (list, tuple, ndarray, Series)):
-            if len(yerr) != self.data_size:
-                raise ArgumentStructureError(
-                    "yerr must be of the same length as x_data and y_data"
-                )
-            if not all(isinstance(y, (float, int)) for y in yerr):
-                raise ArgumentTypeError("All yerr values must be numbers")
+        elif is_sequence(yerr):
+            validate_length(yerr, self.data_size, "yerr")
+            validate_sequence_type(yerr, NUMERIC_TYPES, "yerr")
             self.yerr = yerr
         else:
             raise ArgumentTypeError("yerr must be a number or sequence of numbers")
         return self
 
     def set_ecolor(self, ecolor: EdgeColor):
+        if isinstance(ecolor, str):
+            if (
+                ecolor not in ["face", "none"]
+                and ecolor not in _colors
+                and not re.match(r"^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{8})$", ecolor)
+            ):
+                raise ArgumentTypeError(
+                    f"'ecolor'='{ecolor}' must be 'face', 'none', a valid Matplotlib color string or HEX code."
+                )
+        elif is_sequence(ecolor):
+            validate_sequence_type(ecolor, NUMERIC_TYPES, "ecolor")
+            validate_sequence_length(ecolor, (3, 4), "ecolor")
+        else:
+            raise ArgumentTypeError(
+                "ecolor must be 'face', 'none', a color string or RGB/RGBA values"
+            )
         self.ecolor = ecolor
         return self
 
     def set_capsize(self, capsize: float):
-        if not isinstance(capsize, (int, float)) or capsize < 0:
-            raise ArgumentValueError("capsize must be a non-negative number")
+        validate_type(capsize, NUMERIC_TYPES, "capsize")
+        validate_non_negative(capsize, "capsize")
         self.capsize = capsize
         return self
 
     def set_error_kw(self, **kwargs):
+        validate_type(kwargs, dict, "error_kw")
         self.error_kw = kwargs
         return self
 
     def set_log(self, log: bool):
+        validate_type(log, bool, "log")
         self.log = log
         return self
 
     def set_orientation(self, orientation: Literal["vertical", "horizontal"]):
-        if orientation not in ["vertical", "horizontal"]:
-            raise ArgumentValueError(
-                "orientation must be either 'vertical' or 'horizontal'"
-            )
+        validate_value_in_options(
+            orientation, ["vertical", "horizontal"], "orientation"
+        )
         self.orientation = orientation
         return self
 
     def set_facecolor(self, facecolor: FaceColor):
-        if isinstance(facecolor, str) or (
-            isinstance(facecolor, (list, tuple))
-            and len(facecolor) in [3, 4]
-            and all(isinstance(x, (int, float, integer)) for x in facecolor)
-        ):
-            self.facecolor = facecolor
-        elif isinstance(facecolor, (list, tuple, ndarray, Series)):
-            if len(facecolor) != self.data_size:
-                raise ArgumentStructureError(
-                    "facecolor must be of the same length as x_data and y_data"
+        if isinstance(facecolor, str):
+            if facecolor not in _colors and not re.match(
+                r"^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{8})$", facecolor
+            ):
+                raise ArgumentTypeError(
+                    f"'facecolor'='{facecolor}' must be a valid Matplotlib color string or HEX code."
                 )
-            for fc in facecolor:
-                if not (
-                    isinstance(fc, str)
-                    or (
-                        isinstance(fc, (list, tuple))
-                        and len(fc) in [3, 4]
-                        and all(isinstance(x, (int, float, integer)) for x in fc)
-                    )
-                ):
-                    raise ArgumentTypeError(
-                        "Each facecolor must be a string or RGB/RGBA values."
-                    )
+            self.facecolor = facecolor
+        elif isinstance(facecolor, SEQUENCE_TYPES):
+            if is_sequence(facecolor, check_length=True):
+                validate_length(facecolor, self.data_size, "facecolor")
+                for fc in facecolor:
+                    if isinstance(fc, str):
+                        if fc not in _colors and not re.match(
+                            r"^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{8})$", fc
+                        ):
+                            raise ArgumentTypeError(
+                                f"'facecolor' element '{fc}' must be a valid Matplotlib color string or HEX code."
+                            )
+                    else:
+                        validate_sequence_type(fc, NUMERIC_TYPES, "facecolor")
+                        validate_sequence_length(fc, (3, 4), "facecolor")
+            else:
+                validate_sequence_type(facecolor, NUMERIC_TYPES, "facecolor")
+                validate_sequence_length(facecolor, (3, 4), "facecolor")
             self.facecolor = facecolor
         else:
             raise ArgumentTypeError(
@@ -276,29 +285,48 @@ class BarPlotter(Plotter):
         return self
 
     def set_fill(self, fill: Union[bool, Sequence[bool]]):
-        self.fill = fill
+        if isinstance(fill, bool):
+            validate_type(fill, bool, "fill")
+            self.fill = fill
+        else:
+            validate_type(fill, SEQUENCE_TYPES, "fill")
+            validate_sequence_type(fill, bool, "fill")
+            validate_length(fill, self.data_size, "fill")
+            self.fill = fill
         return self
 
     def set_linestyle(self, linestyle: str):
+        _valid_styles = ["-", "--", "-.", ":", "None", "none", " ", ""]
+        validate_value_in_options(linestyle, _valid_styles, "linestyle")
         self.linestyle = linestyle
         return self
 
     def set_hatch(self, hatch: Union[Sequence[str], str]):
         if isinstance(hatch, str):
-            self.hatch = hatch
-        elif isinstance(hatch, (list, tuple, ndarray, Series)):
-            if len(hatch) != self.data_size:
-                raise ArgumentStructureError(
-                    "hatch must be of the same length as x_data and y_data"
-                )
-            if not all(isinstance(h, str) for h in hatch):
-                raise ArgumentTypeError("All hatch values must be strings")
+            validate_type(hatch, str, "hatch")
             self.hatch = hatch
         else:
-            raise ArgumentTypeError("hatch must be a string or sequence of strings")
+            validate_type(hatch, SEQUENCE_TYPES, "hatch")
+            validate_sequence_type(hatch, str, "hatch")
+            validate_length(hatch, self.data_size, "hatch")
+            self.hatch = hatch
         return self
 
     def set_colormap(self, cmap: Cmap):
+        _valid_cmaps = [
+            "magma",
+            "inferno",
+            "plasma",
+            "viridis",
+            "cividis",
+            "twilight",
+            "twilight_shifted",
+            "turbo",
+        ]
+        if isinstance(cmap, str):
+            validate_value_in_options(cmap, _valid_cmaps, "cmap")
+        else:
+            validate_type(cmap, Colormap, "cmap")
         self.colormap = cmap
         return self
 
