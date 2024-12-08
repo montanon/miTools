8d4285b9a76a4936516fe2640d0b4ab0b8fac1b0
montanon <montagna.p.92@hotmail.com>
2024-12-08 01:24:52 -0300

    feat: add basic textblob init

---
+++ mitools/nlp/textblob/__init__.py
@@ -0,0 +1,6 @@
+import nltk
+
+wordnet = nltk.corpus.wordnet
+Synset = nltk.corpus.wordnet.synset
+Lemma = nltk.corpus.wordnet.lemma
+VERB, NOUN, ADJ, ADV = wordnet.VERB, wordnet.NOUN, wordnet.ADJ, wordnet.ADV
