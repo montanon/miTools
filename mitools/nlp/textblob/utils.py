bf15d7bd58b146ce0dfb8d2ad0c947e28a2759ac
montanon <montagna.p.92@hotmail.com>
2024-12-08 00:38:53 -0300

    feat: add textblob utils

---
+++ mitools/nlp/textblob/utils.py
@@ -0,0 +1,41 @@
+import re
+import string
+from typing import Sequence, Tuple
+
+from nltk.tree import Tree
+
+PUNCTUATION_REGEX = re.compile(f"[{re.escape(string.punctuation)}]")
+
+
+def strip_punctuation(s: str, all: bool = False) -> str:
+    if all:
+        return PUNCTUATION_REGEX.sub("", s.strip())
+    else:
+        return s.strip().strip(string.punctuation)
+
+
+def lowerstrip(s: str, all: bool = False) -> str:
+    return strip_punctuation(s.lower().strip(), all=all)
+
+
+def tree_to_string(tree: Tree, concat: str = " ") -> str:
+    return concat.join([word for (word, tag) in tree])
+
+
+def filter_insignificant(
+    chunk: Sequence[str], tag_suffixes: Tuple[str] = ("DT", "CC", "PRP$", "PRP")
+) -> bool:
+    good = []
+    for word, tag in chunk:
+        ok = True
+        for suffix in tag_suffixes:
+            if tag.endswith(suffix):
+                ok = False
+                break
+        if ok:
+            good.append((word, tag))
+    return good
+
+
+def is_filelike(obj: object) -> bool:
+    return hasattr(obj, "read")
