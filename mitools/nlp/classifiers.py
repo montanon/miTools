b66ac7d2d07551babc8e26a51e132f19bec0178f
montanon <montagna.p.92@hotmail.com>
2024-12-09 12:00:53 -0300

    feat: add NLTKClassifier

--- mitools/nlp/classifiers.py
+++ mitools/nlp/classifiers.py
@@ -1,5 +1,7 @@
 from typing import Callable, Iterable, Tuple
 
+import nltk
+
 from mitools.nlp.typing import BaseString
 from mitools.nlp.utils import basic_extractor, get_words_from_corpus
 from mitools.utils.decorators import cached_property
@@ -36,3 +38,66 @@ class BaseClassifier:
             return self.feature_extractor(text, self.bag_of_words)
         except (TypeError, AttributeError):
             return self.feature_extractor(text)
+
+
+class NLTKClassifier(BaseClassifier):
+    nltk_class = None
+
+    def __init__(
+        self,
+        train_set: Iterable[BaseString],
+        feature_extractor: Callable = None,
+        **kwargs,
+    ):
+        super().__init__(train_set, feature_extractor, **kwargs)
+        self.train_features = [(self.extract_features(d), c) for d, c in self.train_set]
+
+    def __repr__(self):
+        class_name = self.__class__.__name__
+        return f"<{class_name} trained on {len(self.train_set)} instances>"
+
+    @cached_property
+    def classifier(self):
+        try:
+            return self.train()
+        except AttributeError as error:  # nltk_class has not been defined
+            raise ValueError(
+                "NLTKClassifier must have a nltk_class" " variable that is not None."
+            ) from error
+
+    def train(self, *args, **kwargs):
+        try:
+            self.classifier = self.nltk_class.train(
+                self.train_features, *args, **kwargs
+            )
+            return self.classifier
+        except AttributeError as error:
+            raise ValueError(
+                "NLTKClassifier must have a nltk_class" " variable that is not None."
+            ) from error
+
+    def labels(self):
+        return self.classifier.labels()
+
+    def classify(self, text: BaseString):
+        text_features = self.extract_features(text)
+        return self.classifier.classify(text_features)
+
+    def accuracy(self, test_set: Iterable[Tuple[BaseString, bool]]):
+        test_data = test_set
+        test_features = [(self.extract_features(d), c) for d, c in test_data]
+        return nltk.classify.accuracy(self.classifier, test_features)
+
+    def update(self, new_data: Iterable[Tuple[BaseString, bool]], *args, **kwargs):
+        self.train_set += new_data
+        self.bag_of_words.update(get_words_from_corpus(new_data))
+        self.train_features = [(self.extract_features(d), c) for d, c in self.train_set]
+        try:
+            self.classifier = self.nltk_class.train(
+                self.train_features, *args, **kwargs
+            )
+        except AttributeError as error:  # Descendant has not defined nltk_class
+            raise ValueError(
+                "NLTKClassifier must have a nltk_class" " variable that is not None."
+            ) from error
+        return True
