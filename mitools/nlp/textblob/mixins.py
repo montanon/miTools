f1de3943f7d521382f3f33ba225f86773f8fa845
montanon <montagna.p.92@hotmail.com>
2024-12-09 15:43:54 -0300

    feat: renamed keys to call

--- mitools/nlp/textblob/mixins.py
+++ mitools/nlp/textblob/mixins.py
@@ -1,10 +1,15 @@
 import sys
+from abc import ABC, abstractmethod
 
 
-class ComparableMixin:
+class ComparableMixin(ABC):
+    @abstractmethod
+    def comparable_key(self):
+        raise NotImplementedError("comparable_key must be implemented")
+
     def _compare(self, other, method):
         try:
-            return method(self._cmpkey(), other._cmpkey())
+            return method(self.comparable_key(), other.comparable_key())
         except (AttributeError, TypeError):
             return NotImplemented
 
@@ -30,73 +35,76 @@ class ComparableMixin:
 class BlobComparableMixin(ComparableMixin):
     def _compare(self, other, method):
         if isinstance(other, (str, bytes)):
-            return method(self._cmpkey(), other)
+            return method(self.comparable_key(), other)
         return super()._compare(other, method)
 
 
-class StringlikeMixin:
+class StringlikeMixin(ABC):
+    @abstractmethod
+    def string_key(self):
+        raise NotImplementedError("string_key must be implemented")
+
     def __repr__(self):
         class_name = self.__class__.__name__
         text = str(self)
         return f'{class_name}("{text}")'
 
     def __str__(self):
-        return self._strkey()
+        return self.string_key()
 
     def __len__(self):
-        return len(self._strkey())
+        return len(self.string_key())
 
     def __iter__(self):
-        return iter(self._strkey())
+        return iter(self.string_key())
 
     def __contains__(self, sub):
-        return sub in self._strkey()
+        return sub in self.string_key()
 
     def __getitem__(self, index):
         if isinstance(index, int):
-            return self._strkey()[index]  # Just return a single character
+            return self.string_key()[index]
         else:
-            # Return a new blob object
-            return self.__class__(self._strkey()[index])
+            return self.__class__(self.string_key()[index])
 
     def find(self, sub, start=0, end=sys.maxsize):
-        return self._strkey().find(sub, start, end)
+        return self.string_key().find(sub, start, end)
 
     def rfind(self, sub, start=0, end=sys.maxsize):
-        return self._strkey().rfind(sub, start, end)
+        return self.string_key().rfind(sub, start, end)
 
     def index(self, sub, start=0, end=sys.maxsize):
-        return self._strkey().index(sub, start, end)
+        return self.string_key().index(sub, start, end)
 
     def rindex(self, sub, start=0, end=sys.maxsize):
-        return self._strkey().rindex(sub, start, end)
+        return self.string_key().rindex(sub, start, end)
 
     def startswith(self, prefix, start=0, end=sys.maxsize):
-        return self._strkey().startswith(prefix, start, end)
+        return self.string_key().startswith(prefix, start, end)
 
     def endswith(self, suffix, start=0, end=sys.maxsize):
-        return self._strkey().endswith(suffix, start, end)
+        return self.string_key().endswith(suffix, start, end)
 
     def title(self):
-        return self.__class__(self._strkey().title())
+        return self.__class__(self.string_key().title())
 
     def format(self, *args, **kwargs):
-        return self.__class__(self._strkey().format(*args, **kwargs))
+        return self.__class__(self.string_key().format(*args, **kwargs))
 
     def split(self, sep=None, maxsplit=sys.maxsize):
-        return self._strkey().split(sep, maxsplit)
+        return self.string_key().split(sep, maxsplit)
 
     def strip(self, chars=None):
-        return self.__class__(self._strkey().strip(chars))
+        return self.__class__(self.string_key().strip(chars))
 
     def upper(self):
-        return self.__class__(self._strkey().upper())
+        return self.__class__(self.string_key().upper())
 
     def lower(self):
-        return self.__class__(self._strkey().lower())
+        return self.__class__(self.string_key().lower())
 
     def join(self, iterable):
-        return self.__class__(self._strkey().join(iterable))
+        return self.__class__(self.string_key().join(iterable))
 
     def replace(self, old, new, count=sys.maxsize):
-        return self.__class__(self._strkey().replace(old, new, count))
+        return self.__class__(self.string_key().replace(old, new, count))
