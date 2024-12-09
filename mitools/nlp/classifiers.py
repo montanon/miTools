791cd76abb5349c3c0c8b21923e03e96f74795b4
montanon <montagna.p.92@hotmail.com>
2024-12-09 12:06:57 -0300

    feat: add extra classifiers

--- mitools/nlp/classifiers.py
+++ mitools/nlp/classifiers.py
@@ -1,9 +1,15 @@
 from typing import Callable, Iterable, Tuple
 
 import nltk
+from nltk.classify import (
+    DecisionTreeClassifier,
+    MaxentClassifier,
+    NaiveBayesClassifier,
+    PositiveNaiveBayesClassifier,
+)
 
 from mitools.nlp.typing import BaseString
-from mitools.nlp.utils import basic_extractor, get_words_from_corpus
+from mitools.nlp.utils import basic_extractor, contains_extractor, get_words_from_corpus
 from mitools.utils.decorators import cached_property
 
 
@@ -100,4 +106,89 @@ class NLTKClassifier(BaseClassifier):
             raise ValueError(
                 "NLTKClassifier must have a nltk_class" " variable that is not None."
             ) from error
+        return True
+
+
+class NaiveBayesClassifier(NLTKClassifier):
+    nltk_class = NaiveBayesClassifier
+
+    def prob_classify(self, text: BaseString):
+        text_features = self.extract_features(text)
+        return self.classifier.prob_classify(text_features)
+
+    def informative_features(self, *args, **kwargs):
+        return self.classifier.most_informative_features(*args, **kwargs)
+
+    def show_informative_features(self, *args, **kwargs):
+        return self.classifier.show_most_informative_features(*args, **kwargs)
+
+
+class DecisionTreeClassifier(NLTKClassifier):
+    nltk_class = DecisionTreeClassifier
+
+    def pretty_format(self, *args, **kwargs):
+        return self.classifier.pretty_format(*args, **kwargs)
+
+    def pseudocode(self, *args, **kwargs):
+        return self.classifier.pseudocode(*args, **kwargs)
+
+
+class PositiveNaiveBayesClassifier(NLTKClassifier):
+    nltk_class = PositiveNaiveBayesClassifier
+
+    def __init__(
+        self,
+        positive_set: Iterable[BaseString],
+        unlabeled_set: Iterable[BaseString],
+        feature_extractor: Callable = None,
+        positive_prob_prior: float = 0.5,
+    ):
+        self.feature_extractor = (
+            feature_extractor if feature_extractor is not None else contains_extractor
+        )
+        self.positive_set = positive_set
+        self.unlabeled_set = unlabeled_set
+        self.positive_features = [self.extract_features(d) for d in self.positive_set]
+        self.unlabeled_features = [self.extract_features(d) for d in self.unlabeled_set]
+        self.positive_prob_prior = positive_prob_prior
+
+    def __repr__(self):
+        class_name = self.__class__.__name__
+        return (
+            f"<{class_name} trained on {len(self.positive_set)} labeled "
+            f"and {len(self.unlabeled_set)} unlabeled instances>"
+        )
+
+    def train(self):
+        self.classifier = self.nltk_class.train(
+            self.positive_features, self.unlabeled_features, self.positive_prob_prior
+        )
+        return self.classifier
+
+    def update(
+        self,
+        new_positive_data: Iterable[BaseString] = None,
+        new_unlabeled_data: Iterable[BaseString] = None,
+        positive_prob_prior: float = 0.5,
+        *args,
+        **kwargs,
+    ):
+        self.positive_prob_prior = positive_prob_prior
+        if new_positive_data:
+            self.positive_set += new_positive_data
+            self.positive_features += [
+                self.extract_features(d) for d in new_positive_data
+            ]
+        if new_unlabeled_data:
+            self.unlabeled_set += new_unlabeled_data
+            self.unlabeled_features += [
+                self.extract_features(d) for d in new_unlabeled_data
+            ]
+        self.classifier = self.nltk_class.train(
+            self.positive_features,
+            self.unlabeled_features,
+            self.positive_prob_prior,
+            *args,
+            **kwargs,
+        )
         return True
