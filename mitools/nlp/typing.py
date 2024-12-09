68870602adc3db0daa8d38355d7bd99be628ff42
montanon <montagna.p.92@hotmail.com>
2024-12-09 11:36:45 -0300

    feat: add extra typing

--- mitools/nlp/typing.py
+++ mitools/nlp/typing.py
@@ -1,6 +1,7 @@
-from typing import Literal, Union
+from typing import List, Literal, Tuple, Union
 
 PosTag = Literal["tags"]
 PennTag = Literal["tags"]
 WordNetTag = Literal["tags"]
 BaseString = Union[str, bytes]
+SentimentType = Union[Tuple[float, float], Tuple[float, float, List[str]]]
