94ce5a9a932dc8ac894229df6230451720ed934c
montanon <montagna.p.92@hotmail.com>
2024-12-09 15:42:57 -0300

    feat: add wordnet tags to typing

--- mitools/nlp/typing.py
+++ mitools/nlp/typing.py
@@ -1,7 +1,10 @@
 from typing import List, Literal, Tuple, Union
 
+from nltk.corpus import wordnet
+
 PosTag = Literal["tags"]
 PennTag = Literal["tags"]
 WordNetTag = Literal["tags"]
 BaseString = Union[str, bytes]
 SentimentType = Union[Tuple[float, float], Tuple[float, float, List[str]]]
+VERB, NOUN, ADJ, ADV = wordnet.VERB, wordnet.NOUN, wordnet.ADJ, wordnet.ADV
