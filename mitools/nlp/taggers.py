76778c28413ea0e748ffcd51e41ce0f62f76f7dd
montanon <montagna.p.92@hotmail.com>
2024-12-11 18:25:30 -0300

    feat: make tokens tagging explicit and add iterator

--- mitools/nlp/taggers.py
+++ mitools/nlp/taggers.py
@@ -1,21 +1,29 @@
 from abc import ABC, abstractmethod
-from typing import Sequence, Tuple
+from typing import Iterator, Sequence, Tuple
 
 import nltk
 
 from mitools.nlp.en import tag as pattern_tag
 from mitools.nlp.nlp_typing import BaseString, PosTag
-from mitools.nlp.tokenizers import BaseTokenizer, WordTokenizer
 
 
 class BaseTagger(ABC):
     @abstractmethod
-    def tag(self, tokens: Sequence[BaseString]) -> Sequence[Tuple[BaseString, PosTag]]:
+    def tag_tokens(
+        self, tokens: Sequence[BaseString]
+    ) -> Sequence[Tuple[BaseString, PosTag]]:
         pass
+
+    def itag_tokens(
+        self, tokens: Sequence[BaseString]
+    ) -> Iterator[Tuple[BaseString, PosTag]]:
+        return (t for t in self.tag_tokens(tokens))
 
 
 class PatternTagger(BaseTagger):
-    def tag(self, tokens: Sequence[BaseString]) -> Sequence[Tuple[BaseString, PosTag]]:
+    def tag_tokens(
+        self, tokens: Sequence[BaseString]
+    ) -> Sequence[Tuple[BaseString, PosTag]]:
         return pattern_tag(tokens)
 
 
@@ -28,5 +36,7 @@ class NLTKTagger(BaseTagger):
             cls._instance = super().__new__(cls)
         return cls._instance
 
-    def tag(self, tokens: Sequence[BaseString]) -> Sequence[Tuple[BaseString, PosTag]]:
+    def tag_tokens(
+        self, tokens: Sequence[BaseString]
+    ) -> Sequence[Tuple[BaseString, PosTag]]:
         return self._tagger(tokens)
