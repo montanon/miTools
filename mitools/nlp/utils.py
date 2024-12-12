180a901e6948ba8c2dcc73851e18e2f649b0a3f4
montanon <montagna.p.92@hotmail.com>
2024-12-12 15:32:27 -0300

    feat: extend get_words_from_corpus args

--- mitools/nlp/utils.py
+++ mitools/nlp/utils.py
@@ -104,13 +104,30 @@ def sentence_tokenize(
     return sentence_tokenizer.itokenize(text, *args, **kwargs)
 
 
-def get_words_from_corpus(corpus: Iterable[BaseString]) -> Set[BaseString]:
-    def tokenize(words):
+def get_words_from_corpus(
+    corpus: Iterable[BaseString],
+    word_tokenizer: WordTokenizer = None,
+    sentence_tokenizer: SentenceTokenizer = None,
+    include_punctuation: bool = False,
+    *args,
+    **kwargs,
+) -> Set[BaseString]:
+    def tokenize(words, word_tokenizer, sentence_tokenizer, *args, **kwargs):
         if isinstance(words, (str, bytes)):
-            return word_tokenize(words, include_punctuation=False)
+            return word_tokenize(
+                words,
+                word_tokenizer=word_tokenizer,
+                sentence_tokenizer=sentence_tokenizer,
+                include_punctuation=include_punctuation,
+                *args,
+                **kwargs,
+            )
         return words
 
-    all_words = chain.from_iterable(tokenize(words) for words, _ in corpus)
+    all_words = chain.from_iterable(
+        tokenize(words, word_tokenizer, sentence_tokenizer, *args, **kwargs)
+        for words, _ in corpus
+    )
     return set(all_words)
 
 
