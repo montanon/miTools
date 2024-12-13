a8c1bf4da5d4a934be94162ede198d83e1ded247
montanon <montagna.p.92@hotmail.com>
2024-12-13 13:21:19 -0300

    feat: add translate_tags to taggers

--- mitools/nlp/taggers.py
+++ mitools/nlp/taggers.py
@@ -1,10 +1,11 @@
 from abc import ABC, abstractmethod
-from typing import Iterator, Sequence, Tuple, Union
+from typing import Iterator, Literal, Sequence, Tuple, Union
 
 import nltk
 
 from mitools.nlp.en import tag as pattern_tag
 from mitools.nlp.nlp_typing import BaseString, PosTag
+from mitools.nlp.tags_translator import translate_tags
 
 
 class BaseTagger(ABC):
@@ -22,16 +23,23 @@ class BaseTagger(ABC):
 
 class PatternTagger(BaseTagger):
     def tag_tokens(
-        self, tokens: Union[BaseString, Sequence[BaseString]]
+        self,
+        tokens: Union[BaseString, Sequence[BaseString]],
+        tags_format: Literal["penn", "universal", "wordnet"] = "universal",
     ) -> Sequence[Tuple[BaseString, PosTag]]:
         if isinstance(tokens, str):
             tokens = [tokens]
-        return pattern_tag(tokens)
+        tagged_tokens = pattern_tag(tokens)
+        if tags_format != "universal":
+            tagged_tokens = translate_tags(
+                tagged_tokens, source_format="penn", target_format=tags_format
+            )
+        return tagged_tokens
 
 
 class NLTKTagger(BaseTagger):
     _instance = None
-    _tagger = nltk.tag.pos_tag
+    _tagger = staticmethod(nltk.tag.pos_tag)
 
     def __new__(cls):
         if cls._instance is None:
@@ -39,8 +47,15 @@ class NLTKTagger(BaseTagger):
         return cls._instance
 
     def tag_tokens(
-        self, tokens: Union[BaseString, Sequence[BaseString]]
+        self,
+        tokens: Union[BaseString, Sequence[BaseString]],
+        tags_format: Literal["penn", "universal", "wordnet"] = "penn",
     ) -> Sequence[Tuple[BaseString, PosTag]]:
         if isinstance(tokens, str):
             tokens = [tokens]
-        return self._tagger(tokens)
+        tagged_tokens = self._tagger(tokens)
+        if tags_format != "penn":
+            tagged_tokens = translate_tags(
+                tagged_tokens, source_format="penn", target_format=tags_format
+            )
+        return tagged_tokens
