bdb73646541c68796c8e6b7da90d87a508407632
montanon <montagna.p.92@hotmail.com>
2024-12-18 11:24:41 -0300

    feat: add QuantileRegressionModel

--- mitools/regressions/regression_models.py
+++ mitools/regressions/regression_models.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from typing import List, Optional
+from typing import List, Optional, Union
 
 import numpy as np
 import statsmodels.api as sm
@@ -122,6 +122,10 @@ class BaseRegressionModel(ABC):
 
 
 class OLSModel(BaseRegressionModel):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.model_name = "OLS"
+
     def fit(self, add_constant: bool = True, *args, **kwargs):
         if self.formula is not None:
             model = smf.ols(formula=self.formula, data=self.data, *args, **kwargs)
@@ -136,3 +140,77 @@ class OLSModel(BaseRegressionModel):
         self.results = model.fit()
         self.fitted = True
         return self.results
+
+
+class QuantileRegressionModel(BaseRegressionModel):
+    def __init__(
+        self,
+        data: DataFrame,
+        formula: Optional[str] = None,
+        dependent_variable: Optional[str] = None,
+        independent_variables: Optional[List[str]] = None,
+        control_variables: Optional[List[str]] = None,
+        quantiles: Union[float, List[float]] = 0.5,
+    ):
+        super().__init__(
+            data=data,
+            formula=formula,
+            dependent_variable=dependent_variable,
+            independent_variables=independent_variables,
+            control_variables=control_variables,
+        )
+        if isinstance(quantiles, float):
+            self.quantiles = [quantiles]
+        else:
+            self.quantiles = sorted(quantiles)
+        self.model_name = "QuantReg"
+
+    def fit(self, add_constant: bool = True, *args, **kwargs):
+        if self.formula is not None:
+            model = smf.quantreg(formula=self.formula, data=self.data)
+        else:
+            endog = self.data[self.dependent_variable]
+            exog_vars = self.independent_variables + self.control_variables
+            exog = self.data[exog_vars]
+            if add_constant:
+                exog = sm.add_constant(exog)
+            model = sm.QuantReg(endog, exog)
+        self.results = {}
+        for q in self.quantiles:
+            self.results[q] = model.fit(q=q, *args, **kwargs)
+        self.fitted = True
+        return self.results
+
+    def predict(
+        self,
+        new_data: Optional[DataFrame] = None,
+        quantiles: Optional[Union[float, List[float]]] = None,
+    ):
+        if not self.fitted:
+            raise ArgumentValueError("Model not fitted yet")
+        if quantiles is not None:
+            quantiles = [quantiles] if isinstance(quantiles, float) else quantiles
+            quantiles = sorted(quantiles)
+            if any(quantile not in self.quantiles for quantile in quantiles):
+                raise ArgumentValueError(
+                    f"Quantiles {[quantile for quantile in quantiles if quantile not in self.quantiles]} not in 'self.quantiles'={self.quantiles}"
+                )
+            predictions = {q: self.results[q].predict(new_data) for q in self.quantiles}
+            return predictions if len(self.quantiles) > 1 else predictions[quantiles[0]]
+        else:
+            return {q: self.results[q].predict(new_data) for q in self.quantiles}
+
+    def summary(self, quantiles: Optional[Union[float, List[float]]] = None):
+        if not self.fitted:
+            raise ArgumentValueError("Model not fitted yet")
+        if quantiles is not None:
+            quantiles = [quantiles] if isinstance(quantiles, float) else quantiles
+            quantiles = sorted(quantiles)
+            if any(quantile not in self.quantiles for quantile in quantiles):
+                raise ArgumentValueError(
+                    f"Quantiles {[quantile for quantile in quantiles if quantile not in self.quantiles]} not in 'self.quantiles'={self.quantiles}"
+                )
+            summaries = {q: self.results[q].summary() for q in quantiles}
+            return summaries if len(quantiles) > 1 else summaries[quantiles[0]]
+        else:
+            return {q: self.results[q].summary() for q in self.quantiles}
